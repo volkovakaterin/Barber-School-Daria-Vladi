2e59f339ba504e1bc5397bbb06ac5f60ec734c2e
Ekaterina Volkova <volkovakaterin@gmail.com>
2023-07-28 14:54:35 +0300

    [+] верстка блока AboutSchool

--- intro/src/components/AboutSchool/AboutSchool.js
+++ intro/src/components/AboutSchool/AboutSchool.js
@@ -1,23 +1,42 @@
 import  './AboutSchool.css';
 import { NavLink } from 'react-router-dom';
 import imgThreeMaster from '../../img/threeMasterCadrir.jpg';
-import imgThreeMasterMobile from '../../img/threeMasterMobile.jpg'
+import imgThreeMasterMobile from '../../img/threeMasterMobile.jpg';
+import imgDaria from '../../img/DariaMaster-cut.jpg';
 
 
 function AboutSchool() {
     return(
     <div className='about_school'>
-    <h1 className='about_school_title'>О нас</h1>
-    <div className='about_school_content'>
-      <div className='about_school_container_img'><img src={window.screen.width > 640 ? imgThreeMaster : imgThreeMasterMobile} alt='Наша команда'></img></div>
-      <div className='about_school_container_decription'>
-        <div className='about_school_substrate'></div>
-        <p>Команда школы стилистов DARIA VLADY с любовью передаст Вам глубокие знания о красоте и моде, стиле и здоровье. Вы сможете делать людей счастливыми через красивые стрижки, окрашивания и уходы.</p>
+    <div className='about_school_title_wrapper'>
+      <div className='about_school_title_decor'></div>
+      <h1 className='about_school_title'>О нас</h1>
+    </div>
+    <div className='about_school_content_wrapper'>
+      <div className='about_school_content_team'>
+        <div className='about_school_container_img'>
+          <div className='about_school_substrate_team'></div>
+          <img src={window.screen.width > 640 ? imgThreeMaster : imgThreeMasterMobile} alt='Наша команда'></img>
+          </div>
+        <div className='about_school_container_decription'>
+        <div className='about_school_decor_team'></div>
+        <p>Команда школы стилистов <span>DARIA VLADY</span> с любовью передаст Вам глубокие знания о красоте и моде, стиле и здоровье. Вы сможете делать людей счастливыми через красивые стрижки, окрашивания и уходы.</p>
+      </div>
+      </div>
+      <div className='about_school_content_Daria'>
+        <div className='about_school_container_decription'>
+          <div className='about_school_decor_Daria'></div>
         <p>Мы создаем притягательную атмосферу красоты и преображения. Раскрываем уникальность каждого человека, соединяя красоту его внутреннего мира и мира, в котором мы живём.</p>
-        <p>Делаем то, что любим и любим, то, что делаем!</p>
+        <p>Делаем то, что любим и любим то, что делаем!</p>
+      </div>
+        <div className='about_school_container_img'>
+        <div className='about_school_substrate_Daria'></div>
+          <img src={imgDaria} alt='Daria Master'></img></div>
       </div>
     </div>
-    <div className='choose_course_container'>
+    <div className='about_school_btn_container'>
+      <div className='about_school_btn_substrate'></div>
+      <div className='about_school_btn_decor'></div>
                 <NavLink className='navlink_choose_course' to='/prof-courses.html'>
                     <button className='btn_choose_course'>Купить курс в рассрочку</button>
                     </NavLink>
