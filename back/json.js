0287033ac397242cafbbecde92cd1b60619b6af4
Ekaterina Volkova <volkovakaterin@gmail.com>
2023-09-04 16:46:31 +0300

    [*]слияние веток дизайна и рассрочки,правка стилей

--- back/json.js
+++ back/json.js
@@ -2,21 +2,20 @@ const https = require("https");
 const fs = require("fs");
 
 const options = {
-//   key: fs.readFileSync("key.pem"),
-//   cert: fs.readFileSync("cert.pem"),
-//   key: fs.readFileSync("/etc/letsencrypt/live/dariavlady.ru/privkey.pem").toString(),
-//   cert: fs.readFileSync("/etc/letsencrypt/live/dariavlady.ru/cert.pem").toString(),
+  //   key: fs.readFileSync("key.pem"),
+  //   cert: fs.readFileSync("cert.pem"),
   key: fs.readFileSync("/etc/letsencrypt/live/dariavlady.ru/privkey.pem"),
   cert: fs.readFileSync("/etc/letsencrypt/live/dariavlady.ru/cert.pem"),
 };
 
-// const host = "195.2.79.212";
+//const host = "195.2.79.212";
+//const host = 'localhost'
 const host = "dariavlady.ru";
 const port = 8000;
 const TelegramBot = require("node-telegram-bot-api");
 const token = "5656788140:AAHpSCLaLriqkRVF-cWt6qMYdzz4a2wCTng";
 const bot = new TelegramBot(token, { polling: true });
-//const chatID = "300712588"; //? Katya DEV chatID
+const chatIDDev = "300712588"; //? Katya DEV chatID
 const chatID = "293318065"; //? Dasha PROD chatID
 
 let messages = JSON.stringify([]);
@@ -39,7 +38,7 @@ const requestListener = function (req, res) {
 
   const messagesRequest = (address) => {
     console.log("messageRequest");
-    if (address === "/send_registration_form") {
+    if (address === "/send_registration_form") { //отправка формы записи на курс
       console.log("if address");
       console.log(methodReq);
       if (methodReq == "OPTIONS") {
@@ -81,6 +80,44 @@ const requestListener = function (req, res) {
             }
           });
           bot.sendMessage(chatID, message);
+          bot.sendMessage(chatIDDev, message);
+          res.writeHead(200);
+          res.end(messages);
+        });
+      }
+    } else if (address === "/open_credit_form") { //открытие формы заявки на рассрочку
+      if (methodReq == "OPTIONS") {
+        console.log("options");
+        res.setHeader("200", "OK");
+        res.setHeader(
+          "Access-Control-Allow-Methods",
+          "PUT, PATCH, DELETE, POST"
+        );
+        res.setHeader(
+          "Access-Control-Allow-Headers",
+          "API-Key, Content-Type, If-Modified-Since, Cache-Control"
+        );
+        res.setHeader("Access-Control-Max-Age", "86400");
+        res.writeHead(200);
+        res.end();
+      } else if (methodReq == "POST") {
+        console.log("post");
+        let data = "";
+        req.on("data", function (chunk) {
+          data += chunk.toString();
+        });
+        req.on("end", function () {
+          let message;
+          message = `Форма заявки на рассрочку на курс ${
+            JSON.parse(data).name
+          }, стасус: ${JSON.parse(data).status}`;
+          bot.on("message", (msg) => {
+            var Hi = "hi";
+            if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
+              bot.sendMessage(msg.chat.id, "Hello dear user");
+            }
+          });
+          bot.sendMessage(chatIDDev, message);
           res.writeHead(200);
           res.end(messages);
         });
@@ -96,81 +133,5 @@ const requestListener = function (req, res) {
 
 const server = https.createServer(options, requestListener);
 server.listen(port, host, () => {
-  console.log(`Server is running on https://${host}:${port}`)});
-
-// const http = require("http");
-// const host = 'localhost';
-// const port = 8000;
-// const TelegramBot = require('node-telegram-bot-api');
-// const token = '5656788140:AAHpSCLaLriqkRVF-cWt6qMYdzz4a2wCTng';
-// const bot = new TelegramBot(token, { polling: true });
-//// const chatID = "300712588"; //? Kata DEV chatID
-// //const chatID = "293318065"; //? Dasha PROD chatID
-
-// let messages = JSON.stringify([]);
-
-// const requestListener = function (req, res) {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, application/json, accept');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-
-//     let methodReq = req.method;
-//     let address = req.url;
-
-//     console.log(req.url);
-
-//     const messagesRequest = (address) => {
-//         console.log('messageRequest');
-//         if (address === "/send_registration_form") {
-//             console.log('if address');
-//             console.log(methodReq);
-//             if (methodReq == "OPTIONS") {
-//                 console.log('options');
-//                 res.setHeader('200', 'OK');
-//                 res.setHeader('Access-Control-Allow-Methods', 'PUT, PATCH, DELETE, POST');
-//                 res.setHeader('Access-Control-Allow-Headers', 'API-Key, Content-Type, If-Modified-Since, Cache-Control');
-//                 res.setHeader('Access-Control-Max-Age', '86400');
-//                 res.writeHead(200);
-//                 res.end();
-//             } else if (methodReq == "POST") {
-//                 console.log('post');
-//                 let data = '';
-//                 req.on('data', function (chunk) {
-//                     data += chunk.toString();
-//                 });
-//                 req.on('end', function () {
-//                     console.log(JSON.parse(data));
-//                     let obj = JSON.parse(messages);
-//                     obj.push(JSON.parse(data));
-//                     messages = JSON.stringify(obj);
-//                     let message;
-//                     if(JSON.parse(data).message.course === '') {
-//                         message = `Пользователь ${JSON.parse(data).message.name} заказал звонок на номер ${JSON.parse(data).message.phone}` 
-//                     } else message = `Пользователь ${JSON.parse(data).message.name}  с номером телефона ${JSON.parse(data).message.phone} записался на курс ${JSON.parse(data).message.course}`;
-//                     console.log(message);
-//                     bot.on('message', (msg) => {
-//                         var Hi = "hi";
-//                         if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
-//                             bot.sendMessage(msg.chat.id, "Hello dear user");
-//                         }
-//                     });
-//                     bot.sendMessage(chatID, message);
-//                     res.writeHead(200);
-//                     res.end(messages);
-//                 });
-//             }
-//         } else {
-//             res.writeHead(404);
-//             res.end(JSON.stringify({ error: "Resource not found" }));
-//         }
-//     }
-
-//     messagesRequest(address);
-// };
-
-
-
-// const server = http.createServer(requestListener);
-// server.listen(port, host, () => {
-//     console.log(`Server is running on http://${host}:${port}`);
-// });
+  console.log(`Server is running on https://${host}:${port}`);
+});
--- intro/src/redux/store.js
+++ intro/src/redux/store.js
@@ -1,13 +1,15 @@
 import {combineReducers, legacy_createStore} from "redux";
 import coursesReducer from "./coursesReducer";
 import formReducer from './formReducer';
+import creditFormReducer from './creditFormReducer';
 
 
 function configureStore(state) {
     return legacy_createStore(
         combineReducers({
           courses: coursesReducer,
-          form: formReducer
+          form: formReducer,
+          creditForm: creditFormReducer
          // ....
          // ....   
         })
